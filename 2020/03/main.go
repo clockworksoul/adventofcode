3265626172b7bb152c86c3871b4f76c84714cd05
Matthew Titmus <matthew.titmus@gmail.com>
2020-12-03 07:07:04 -0500

    Day 3

---
+++ 2020/03/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var trees []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if err != nil {
+			log.Fatalf("parse failure: \"%s\"", txt)
+		}
+		trees = append(trees, txt)
+	}
+
+	fmt.Println(countTrees(trees, 1, 1) *
+		countTrees(trees, 3, 1) *
+		countTrees(trees, 5, 1) *
+		countTrees(trees, 7, 1) *
+		countTrees(trees, 1, 2))
+}
+
+func countTrees(trees []string, dx, dy int) int {
+	count := 0
+
+	for x, y := 0, 0; y < len(trees); x, y = x+dx, y+dy {
+		if trees[y][x%len(trees[y])] == '#' {
+			count++
+		}
+	}
+
+	return count
+}
