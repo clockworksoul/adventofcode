e872d0b4ca91bae1720af77d0b296f7e40faba96
Matthew Titmus <matthew.titmus@gmail.com>
2020-12-14 10:44:51 -0500

    Day 14

--- 2020/14/main.go
+++ 2020/14/main.go
@@ -7,52 +7,113 @@ import (
 	"github.com/clockworksoul/adventofcode"
 )
 
-func main() {
-	memory := map[string]int64{}
-	mask := bitmask{}
+const input = "input.txt"
 
-	adventofcode.MustIngestFile("input.txt", func(line string) {
-		parts := strings.Split(line, " = ")
-		if parts[0] == "mask" {
-			mask = newBitmask(parts[1])
-		} else {
-			val := mask.mask(adventofcode.MustParseInt64(parts[1]))
-			memory[parts[0]] = val
-			fmt.Println(parts[0], val)
-		}
-	})
+const bits = 36
 
+type memory map[int64]int64
+
+func (m memory) sum() int64 {
 	var sum int64
-	for _, v := range memory {
+	for _, v := range m {
 		sum += v
 	}
-	fmt.Println(sum)
+	return sum
 }
 
 type bitmask struct {
 	ones   int64
 	zeroes int64
+	xes    int64
 }
 
 func newBitmask(mask string) bitmask {
-	var ones, zeroes int64
+	var ones, zeroes, xes int64
 
 	for _, c := range mask {
 		ones <<= 1
 		zeroes <<= 1
+		xes <<= 1
 
-		if c == '1' {
+		switch c {
+		case '1':
 			ones |= 1
-		} else if c == '0' {
+		case '0':
 			zeroes |= 1
+		case 'X':
+			xes |= 1
 		}
 	}
 
-	return bitmask{ones, zeroes}
+	return bitmask{ones, zeroes, xes}
 }
 
-func (b bitmask) mask(v int64) int64 {
+func (b bitmask) mask(v int64, countZeroes bool) int64 {
 	v |= b.ones
-	v &= ^b.zeroes
+	if countZeroes {
+		v &= ^b.zeroes
+	}
 	return v
+}
+
+func (b bitmask) xmasks() map[int64]bool {
+	masks := map[int64]bool{0: true}
+
+	for i := 0; i <= bits; i++ {
+		var m int64 = 1 << i
+
+		if b.xes&m == m {
+			for mask := range masks {
+				masks[mask|m] = true
+			}
+		}
+	}
+
+	return masks
+}
+
+func (b bitmask) maskAddress(addr int64) map[int64]bool {
+	masks := b.xmasks()
+	addrs := map[int64]bool{}
+
+	masked := b.mask(addr, false) & ^b.xes
+
+	for m := range masks {
+		addrs[masked|m] = true
+	}
+
+	return addrs
+}
+
+func parseMemoryLine(line string) (int64, int64) {
+	parts := strings.Split(line, " = ")
+
+	s := strings.Replace(parts[0], "mem[", "", 1)
+	s = strings.Replace(s, "]", "", 1)
+	mem := adventofcode.MustParseInt64(s)
+	value := adventofcode.MustParseInt64(parts[1])
+	return mem, value
+}
+
+func main() {
+	memory1, memory2 := memory{}, memory{}
+	mask := bitmask{}
+
+	adventofcode.MustIngestFile(input, func(line string) {
+		if line[0:4] == "mask" {
+			mask = newBitmask(line[7:])
+		} else {
+			mem, val := parseMemoryLine(line)
+
+			memory1[mem] = mask.mask(val, true)
+
+			addrs := mask.maskAddress(mem)
+			for addr := range addrs {
+				memory2[addr] = val
+			}
+		}
+	})
+
+	fmt.Println("Star One:", memory1.sum())
+	fmt.Println("Star Two:", memory2.sum())
 }
---
+++ 2020/14/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXExpansion(t *testing.T) {
+	fmt.Println(strconv.FormatInt(42, 2))
+	// bitmask := newBitmask("000000000000000000000000000000X1001X")
+
+}
+
+func TestMakeXMasks(t *testing.T) {
+	// fmt.Println(strconv.FormatInt(42, 2))
+	bitmask := newBitmask("000000000000000000000000000000X1001X")
+
+	masks := bitmask.xmasks()
+
+	assert.Len(t, masks, 4)
+	assert.Contains(t, masks, int64(0b000000))
+	assert.Contains(t, masks, int64(0b000001))
+	assert.Contains(t, masks, int64(0b100000))
+	assert.Contains(t, masks, int64(0b100001))
+}
+
+func TestMaskAddress(t *testing.T) {
+	// fmt.Println(strconv.FormatInt(42, 2))
+	bitmask := newBitmask("000000000000000000000000000000X1001X")
+
+	addrs := bitmask.maskAddress(42)
+
+	t.Log(addrs)
+
+	assert.Len(t, addrs, 4)
+	assert.Contains(t, addrs, int64(26))
+	assert.Contains(t, addrs, int64(27))
+	assert.Contains(t, addrs, int64(58))
+	assert.Contains(t, addrs, int64(59))
+}
