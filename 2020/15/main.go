220e9c8086d02278dcee75d486dbb189707ef432
Matthew Titmus <matthew.titmus@gmail.com>
2020-12-15 06:57:25 -0500

    Day 15

--- 2020/15/main.go
+++ 2020/15/main.go
@@ -1,11 +1,46 @@
 package main
 
-import (
-	"github.com/clockworksoul/adventofcode"
+import "fmt"
+
+var (
+	input = []int{9, 3, 1, 0, 8, 4}
 )
 
 func main() {
-	adventofcode.MustIngestFile("input.txt", func(line string) {
+	var spoken []int
 
-	})
+	spoken = speak(input, 2020)
+	fmt.Println("Star One:", spoken[len(spoken)-1])
+
+	spoken = speak(input, 30000000)
+	fmt.Println("Star Two:", spoken[len(spoken)-1])
+}
+
+func speak(input []int, max int) []int {
+	speak := []int{}
+	spoken := map[int][]int{}
+	last := 0
+
+	for i, v := range input {
+		last = v
+		speak = append(speak, v)
+		spoken[v] = append(spoken[v], i)
+	}
+
+	for i := len(input); i < max; i++ {
+		before, ok := spoken[last]
+
+		if !ok || len(before) == 1 {
+			last = 0
+			speak = append(speak, last)
+			spoken[last] = append(spoken[last], i)
+		} else {
+			l := len(before)
+			last = before[l-1] - before[l-2]
+			speak = append(speak, last)
+			spoken[last] = append(spoken[last], i)
+		}
+	}
+
+	return speak
 }
